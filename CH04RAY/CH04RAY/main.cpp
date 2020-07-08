cb9c2c92afbbd26c13dc0d74f639c4e18662294f
Brian English <baeng72@gmail.com>
2020-07-08 19:27:52 +1000

    Add CH04RAY to git

---
+++ CH04RAY/CH04RAY/main.cpp
@@ -0,0 +1,43 @@
+#include "ray.h"
+#include "color.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+
+color ray_color(const ray& r) {
+	vec3 unit_direction = unit_vector(r.direction());
+	auto t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+int main() {
+	const auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 384;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+	auto viewport_height = 2.0;
+	auto viewport_width = aspect_ratio * viewport_height;
+	auto focal_length = 1.0;
+
+	auto origin = point3(0, 0, 0);
+	auto horizontal = vec3(viewport_width, 0, 0);
+	auto vertical = vec3(0, viewport_height, 0);
+	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+	unsigned* imageData = (unsigned*)malloc(image_width * image_height * sizeof(unsigned int));//rgb data
+	for (int j = 0; j < image_height; ++j) {
+		unsigned y = image_height - j - 1;
+		for (int i = 0; i < image_width; ++i) {
+			auto u = double(i) / (image_width - 1);
+			auto v = double(j) / (image_height - 1);
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+			color pixel_color = ray_color(r);
+			unsigned* p = imageData + y * image_width + i;
+			write_color(p, pixel_color);
+		}
+	}
+	stbi_write_jpg("image.jpg", image_width, image_height, 4, imageData, 100);
+
+	free(imageData);
+
+
+}
