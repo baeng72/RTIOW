1ad02b8814777ba48cbb0f36dcbe7e36ea2018d0
Brian English <baeng72@gmail.com>
2020-07-09 19:57:28 +1000

    Add to git

---
+++ CH07ANTIALIASING/CH07ANTIALIASING/main.cpp
@@ -0,0 +1,54 @@
+#include "rtweekend.h"
+#include "color.h"
+#include "camera.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+color ray_color(const ray& r, const hittable& world) {
+	hit_record rec;
+	if (world.hit(r, 0, infinity, rec)) {
+		return 0.5 * (rec.normal + color(1, 1, 1));
+	}
+	vec3 unit_direction = unit_vector(r.direction());
+	auto t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+
+int main() {
+	const auto aspect_ratio = 16.0 / 9.0;
+	const int image_width = 384;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+	const int samples_per_pixel = 100;
+
+	hittable_list world;
+
+	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+	
+	camera cam;
+
+
+	unsigned* imageData = (unsigned*)malloc(image_width * image_height * sizeof(unsigned int));//rgb data
+	for (int j = 0; j < image_height; ++j) {
+		unsigned y = image_height - j - 1;
+		for (int i = 0; i < image_width; ++i) {
+			color pixel_color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; ++s) {
+				auto u = (i + random_double()) / (image_width - 1);
+				auto v = (j + random_double()) / (image_height - 1);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world);
+			}
+			unsigned* p = imageData + y * image_width + i;
+			write_color(p, pixel_color,samples_per_pixel);
+		}
+	}
+	stbi_write_jpg("image.jpg", image_width, image_height, 4, imageData, 100);
+
+	free(imageData);
+
+}
