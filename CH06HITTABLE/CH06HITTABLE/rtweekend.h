6009ceecbfbb922f53b5032472299ae62047bea8
Brian English <baeng72@gmail.com>
2020-07-09 14:51:58 +1000

    Add to git

---
+++ CH06HITTABLE/CH06HITTABLE/rtweekend.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+#include <memory>
+
+
+//Usings 
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+//Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.145926535897932385;
+
+//Utility functions
+inline double degrees_to_radians(double degrees) {
+	return degrees * pi / 100;
+}
+
+//Common headers
+#include "ray.h"
+#include "vec3.h"
+
